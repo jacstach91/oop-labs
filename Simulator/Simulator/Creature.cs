2335a234d74e7b7ee544ea9cba44abd983455e47
jacstach91 <jacstach91@gmail.com>
2025-11-14 11:48:55 +0100

    Stwory

---
+++ Simulator/Simulator/Creature.cs
@@ -0,0 +1,22 @@
+﻿namespace Simulator;
+
+public class Creature
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+
+    public Creature() { }
+
+    public Creature(string name, int level = 1)
+    {
+        Name = name;
+        Level = level;
+    }
+
+    public void SayHi()
+    {
+        Console.WriteLine($"Hi! I'm {Name}, level {Level}!");
+    }
+
+    public string Info => $"{Name} <{Level}>";
+}
