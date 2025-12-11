19d14160b91a76c2297649c1015e1583db08dd27
jacstach91 <jacstach91@gmail.com>
2025-12-12 00:47:00 +0100

    SimConsole

---
+++ SimConsole/MapVisualizer.cs
@@ -0,0 +1,56 @@
+﻿using Simulator.Maps;
+
+namespace SimConsole
+{
+    public class MapVisualizer
+    {
+        private readonly Map _map;
+
+        public MapVisualizer(Map map)
+        {
+            _map = map;
+        }
+
+        public void Draw()
+        {
+            int width = _map.SizeX;
+            int height = _map.SizeY;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write(Box.TopLeft);
+            for (int x = 0; x < width - 1; x++) Console.Write($"{Box.Horizontal}{Box.TopMid}");
+            Console.WriteLine($"{Box.Horizontal}{Box.TopRight}");
+
+            for (int y = height - 1; y >= 0; y--)
+            {
+                if (y < height - 1)
+                {
+                    Console.Write(Box.MidLeft);
+                    for (int x = 0; x < width - 1; x++) Console.Write($"{Box.Horizontal}{Box.Cross}");
+                    Console.WriteLine($"{Box.Horizontal}{Box.MidRight}");
+                }
+
+                Console.Write(Box.Vertical);
+                for (int x = 0; x < width; x++)
+                {
+                    var creatures = _map.At(x, y);
+                    char c = ' ';
+                    if (creatures != null && creatures.Count > 0)
+                    {
+                        c = creatures.Count > 1 ? 'X' : creatures[0].Symbol;
+                    }
+                    Console.Write($"{c}{Box.Vertical}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.Write(Box.BottomLeft);
+            for (int x = 0; x < width - 1; x++) Console.Write($"{Box.Horizontal}{Box.BottomMid}");
+            Console.WriteLine($"{Box.Horizontal}{Box.BottomRight}");
+
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ SimConsole/Program.cs
@@ -0,0 +1,40 @@
+﻿using Simulator;
+using Simulator.Maps;
+using System.Text;
+
+namespace SimConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        SmallSquareMap map = new(5);
+        List<Creature> creatures = [new Orc("Gorbag"), new Elf("Elandor")];
+        List<Point> points = [new(2, 2), new(3, 1)];
+        string moves = "dlrludl";
+
+        Simulation simulation = new(map, creatures, points, moves);
+        MapVisualizer mapVisualizer = new(simulation.Map);
+
+        Console.WriteLine("SIMULATION!\n");
+        Console.WriteLine("Starting positions:");
+        mapVisualizer.Draw();
+        int turnNumber = 1;
+
+        int turn = 1;
+        while (!simulation.Finished)
+        {
+            Creature current = simulation.CurrentCreature;
+            string moveName = simulation.CurrentMoveName;
+            Point previousPoint = current.Position;
+            simulation.Turn();
+
+            Console.WriteLine($"TURN {turn}");
+            Console.WriteLine($"{current.GetType().Name.ToUpper()}: {current.Info} {previousPoint} goes {moveName}:");
+            mapVisualizer.Draw();
+
+            turn++;
+        }
+    }
+}
